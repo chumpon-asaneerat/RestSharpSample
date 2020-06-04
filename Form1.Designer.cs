145b53dd2461575771749cd000ef67a05a07b3ba
chumpon-asaneerat <chumponsenate@gmail.com>
2020-06-04 18:12:41 +0700

    Updater 2020-06-04 #1 - Add test code for call rest api with RestSharp

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace RestSharpSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdExecute = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstUsers = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // cmdExecute
+            // 
+            this.cmdExecute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdExecute.Location = new System.Drawing.Point(700, 15);
+            this.cmdExecute.Name = "cmdExecute";
+            this.cmdExecute.Size = new System.Drawing.Size(75, 23);
+            this.cmdExecute.TabIndex = 0;
+            this.cmdExecute.Text = "Execute";
+            this.cmdExecute.UseVisualStyleBackColor = true;
+            this.cmdExecute.Click += new System.EventHandler(this.cmdExecute_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUrl.Location = new System.Drawing.Point(79, 15);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(615, 22);
+            this.txtUrl.TabIndex = 1;
+            this.txtUrl.Text = "http://localhost:3000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Url:";
+            // 
+            // lstUsers
+            // 
+            this.lstUsers.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstUsers.FormattingEnabled = true;
+            this.lstUsers.IntegralHeight = false;
+            this.lstUsers.ItemHeight = 16;
+            this.lstUsers.Location = new System.Drawing.Point(30, 52);
+            this.lstUsers.Name = "lstUsers";
+            this.lstUsers.Size = new System.Drawing.Size(745, 386);
+            this.lstUsers.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstUsers);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.cmdExecute);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdExecute;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstUsers;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+
+using RestSharp;
+//using RestSharp.Authenticators;
+
+namespace RestSharpSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdExecute_Click(object sender, EventArgs e)
+        {
+            var url = txtUrl.Text;
+            var client = new RestClient(url);
+            //client.Authenticator = new HttpBasicAuthenticator("username", "password");
+            var request = new RestRequest();
+            request.Resource = "api/users";
+            request.Method = Method.GET;
+            /*
+            var param = new MyClass { IntData = 1, StringData = "test123" };
+            request.AddJsonBody(param);
+            */
+            var response = client.Get<NResult<List<Staff>>>(request);
+            var json = response.Data;
+            //Console.WriteLine(json.data);
+            lstUsers.ValueMember = "staffId";
+            lstUsers.DisplayMember = "staffName";
+            lstUsers.DataSource = json.data;
+        }
+    }
+
+    public class Staff
+    {
+        public string staffId { get; set; }
+        public string staffName { get; set; }
+    }
+
+    public class NError
+    {
+        public bool hasError { get; set; }
+        public int errNum { get; set; }
+        public string errMsg { get; set; }
+    }
+
+    public class NResult<T>
+        where T:class
+    {
+        public T data { get; set; }
+        public NError errors { get; set; }
+    }
+}
